d23a5cb230b9cb8ad79ab00fc17d3340ec6fff8d
Tirezada <christian.espindula@gmail.com>
2026-05-05 20:24:28 -0300

    Exception

--- aula10/exemplo5.py
+++ aula10/exemplo5.py
@@ -1,4 +1,4 @@
-#TRATAMENTO COM FINALLY
+#TRATAMENTO COM EXCEPTION
 print("=== Cálculo de Produtividade ===")
 
 try:
@@ -6,14 +6,13 @@ try:
     funcionarios = int(input("Total de Funcionários: "))
     
     media_por_funcionario = total_produzido / funcionarios
-except (ValueError, TypeError):
-    print('O valor precisa ser numérico')
-except ZeroDivisionError:
-    print("Erro! Informe um número válido de funcionários!")
+#TRATA O ERRO DE UMA FORMA GERAL, INDICANDO QUAL ERRO FOI
+except Exception as e:
+    print(f'Ops! Erro do tipo: \n{e}')
 except KeyboardInterrupt:
     print("\n\nO usuário finalizou o programa!")
+
 else:
     print(f'Média por funcionário: {media_por_funcionario:.2f}')
-#SERÁ EXECUTADO SE DER ERRO OU NÃO
 finally:
     print("\n=== Programa encerrado ===")
